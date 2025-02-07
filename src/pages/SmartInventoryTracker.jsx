9e8b7aa8c9fcf025e63b9fc8cbbc8fdabfe5d9f8
Dana Delgado <danamdelgadog@gmail.com>
2025-02-07 11:14:26 -0800

    fix import in smartinventorytracker

--- src/pages/SmartInventoryTracker.jsx
+++ src/pages/SmartInventoryTracker.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { apiCall } from './api';
+import { useState, useEffect } from 'react';
+import { apiCall } from './services/api';
 import './SmartInventoryTracker.css';
 
 const SmartInventoryTracker = () => {
